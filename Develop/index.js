07f8cd806cd50c89b1185ad04ee83ea3c07f570d
Brice Boutet <boutetlb@gmail.com>
2020-04-26 01:33:32 -0400

    data generateReadme() and WriteToFile() functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,42 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
+const questions = [{
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username:"
+    },
+    {
+        type: "input",
+        name: "repository",
+        message: "Enter a repository name:"
+    }
 ];
 
-function writeToFile(fileName, data) {
+function promptUser() {
+    return inquirer.prompt(questions);
 }
 
-function init() {
+function generateReadme(answers) {
+    return `${answers.github} , ${answers.repository}`
+}
 
+
+function writeToFile(fileName, data) {
+    try {
+        return writeFileAsync(fileName, data);
+        console.log("Successfully wrote to README.md");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function init() {
+    const answers = await promptUser();
+    const readme = generateReadme(answers);
+    writeToFile("README.md", readme);
 }
 
 init();
