3d598cc2d252d2688be7984032d88099471b89e9
Brice Boutet <boutetlb@gmail.com>
2020-04-27 00:07:16 -0400

    inquirer.prompt questions update

--- Develop/index.js
+++ Develop/index.js
@@ -4,22 +4,52 @@ const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const api = require("./utils/api");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-const question = [{
-    type: "input",
-    name: "github",
-    message: "Enter your GitHub Username:"
-}, ];
+const questions = [{
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username:"
+    }, {
+        type: "input",
+        name: "title",
+        message: "Enter your project title:"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter your project description:"
+    },
+    {
+        type: "input",
+        name: "tableOfContents",
+        message: "Enter the table table of contents:"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Enter your installation guideline:"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Enter your project contributing:"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Enter project tests tests:"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Enter projects questions:"
+    },
+];
 
 function promptUser() {
-    return inquirer.prompt(question);
+    return inquirer.prompt(questions);
 }
-
-
-function generateReadme(answers) {
-    return `${answers.github} , ${answers.repository}`
-}
-
 
 function writeToFile(fileName, data) {
     try {
@@ -32,9 +62,10 @@ function writeToFile(fileName, data) {
 
 async function init() {
     const answers = await promptUser();
-    const readme = generateReadme(answers);
+    console.log(answers)
     const username = answers.github;
     const githubApi = await api.getUser(username);
+    console.log(githubApi)
     writeToFile("README.md", githubApi);
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,16 @@
 function generateMarkdown(data) {
-  return `
+    return `
 # ${data.title}
-
+# ${data.description}
+# ${data.TableOfContents}
+# ${data.installation}
+# ${data.usage}
+# ${data.licence}
+# ${data.contributing}
+# ${data.tests}
+# ${data.questions}
+# ${data.userPicture}
+# ${data.userEmail}
 `;
 }
 
